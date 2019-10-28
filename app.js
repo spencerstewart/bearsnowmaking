d2ff5e95deaac7249d8e8a9e1d513b6ce7af4ef3
Spencer Stewart <sstewart@bbmr.com>
2019-10-28 08:44:39 -0700

    Add compression package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,8 +29,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// For production, compress http response
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
